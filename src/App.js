cae66ff324631f6fb12e202c7c6cdb549c033a09
Dylan Barber <dylan.barber@logicdrop.com>
2022-06-28 22:37:21 -0400

    Adding in hooks, this may or may not work..... More so leaning towards not

--- src/App.js
+++ src/App.js
@@ -1,12 +1,94 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { numberOfTeethFormula, useNumberOfTeethFormula, usePitchDiameterFormula } from "./helpers/formulas";
 
 const App = () => {
+  const [numberOfTeeth, setNumberOfTeeth] = useState("");
+  const [pitchDiameter, setPitchDiameter] = useState("");
+  const [diametralPitch, setDiametralPitch] = useState("");
+  const [addendum, setAddendum] = useState("");
+  const [dedendum, setDedendum] = useState("");
+  const [outsideDiameter, setOutsideDiameter] = useState("");
+  const [rootDiameter, setRootDiameter] = useState("");
+  const [baseCircle, setBaseCircle] = useState("");
+  const [circularPitch, setCircularPitch] = useState("");
+  const [circularThickness, setCircularThickness] = useState("");
+
+  // useEffect(() => {
+  //   if (pitchDiameter !== "" && diametralPitch !== "") {
+  //     setNumberOfTeeth(numberOfTeethFormula(pitchDiameter, diametralPitch));
+  //   }
+  // }, [pitchDiameter, diametralPitch]);
+
+  usePitchDiameterFormula(numberOfTeeth, diametralPitch, setPitchDiameter)
+  useNumberOfTeethFormula(pitchDiameter, diametralPitch, outsideDiameter, numberOfTeeth, setNumberOfTeeth)
 
   return (
     <div className="App">
-      <h1>Test</h1>
+      <div className="inputsContainer">
+        <h1>Gear Calculator I Guess</h1>
+        <label>Number of teeth</label>
+        <input
+          value={numberOfTeeth}
+          onChange={(e) => setNumberOfTeeth(e.target.value)}
+        ></input>
+
+        <label>Pitch diameter</label>
+        <input
+          value={pitchDiameter}
+          onChange={(e) => setPitchDiameter(e.target.value)}
+        ></input>
+
+        <label>Diametral pitch</label>
+        <input
+          value={diametralPitch}
+          onChange={(e) => setDiametralPitch(e.target.value)}
+        ></input>
+
+        <label>Addendum</label>
+        <input
+          value={addendum}
+          onChange={(e) => setAddendum(e.target.value)}
+        ></input>
+
+        <label>Dedendum</label>
+        <input
+          value={dedendum}
+          onChange={(e) => setDedendum(e.target.value)}
+        ></input>
+
+        <label>Outside diameter</label>
+        <input
+          value={outsideDiameter}
+          onChange={(e) => setOutsideDiameter(e.target.value)}
+        ></input>
+
+        <label>Root diameter</label>
+        <input
+          value={rootDiameter}
+          onChange={(e) => setRootDiameter(e.target.value)}
+        ></input>
+
+        <label>Base circle</label>
+        <input
+          value={baseCircle}
+          onChange={(e) => setBaseCircle(e.target.value)}
+        ></input>
+
+        <label>Circular pitch</label>
+        <input
+          value={circularPitch}
+          onChange={(e) => setCircularPitch(e.target.value)}
+        ></input>
+
+        <label>Circular thickness</label>
+        <input
+          value={circularThickness}
+          onChange={(e) => setCircularThickness(e.target.value)}
+        ></input>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/helpers/formulas.js
+++ src/helpers/formulas.js
@@ -1,11 +1,18 @@
-//N = number of teeth on gear
-//D = pitch diameter
-//P = Diametral Pitch (Gear Size)
+import { useEffect } from "react";
+
 
 import { add, cos, divide, multiply, subtract } from "mathjs"
 
 
 export const pitchDiameterFormula = (numberOfTeeth, diametralPitch) => divide(numberOfTeeth, diametralPitch)
+
+export const usePitchDiameterFormula = (numberOfTeeth, diametralPitch, pitchDiameter, setPitchDiameter) => {
+    useEffect(() => {
+        if (numberOfTeeth !== "" && diametralPitch !== "" && pitchDiameter === "") {
+            setPitchDiameter(pitchDiameterFormula(numberOfTeeth, diametralPitch));
+        }
+      }, [numberOfTeeth, diametralPitch, setPitchDiameter]);
+}
 
 export const diametralPitchFormula = (numberOfTeeth, pitchDiameter) => divide(numberOfTeeth, pitchDiameter)
 
@@ -23,6 +30,21 @@ export const circularPitchFormula = (diametralPitch) => divide(3.1416, diametral
 // God why doesn't JS support overloading? :(
 export const circularPitchFormulaTwoArgs = (numberOfTeeth, pitchDiameter) => divide(multiply(3.1416, pitchDiameter), numberOfTeeth)
 
-circularThicknessFormula = (diametralPitch) => divide(1.57, diametralPitch)
+export const circularThicknessFormula = (diametralPitch) => divide(1.57, diametralPitch)
 
 export const circularThicknessFormulaTwoArgs = (pitchDiameter, numberOfTeeth) => divide(multiply(3.1416, pitchDiameter), multiply(2, numberOfTeeth))
+
+export const numberOfTeethFormula = (pitchDiameter, diametralPitch) => multiply(pitchDiameter, diametralPitch)
+
+export const numberOfTeethFormula2 = (outerDiameter, diametralPitch) => add(multiply(outerDiameter, diametralPitch), 2)
+
+export const useNumberOfTeethFormula = (pitchDiameter, diametralPitch, outerDiameter, numberOfTeeth, setNumberOfTeeth) => {
+    useEffect(() => {
+    if (pitchDiameter !== "" && diametralPitch !== "" && numberOfTeeth === "") {
+      setNumberOfTeeth(numberOfTeethFormula(pitchDiameter, diametralPitch));
+    } else if (pitchDiameter !== "" && outerDiameter !== "") {
+      setNumberOfTeeth(numberOfTeethFormula2(outerDiameter, diametralPitch));
+    }
+  }, [pitchDiameter, diametralPitch, outerDiameter, setNumberOfTeeth]);
+
+}
